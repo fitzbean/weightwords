91eadcd8b45c4ba3d5806c718c31a46ea1926d0d
Brad Lima <blima@grovergaming.com>
2026-01-07 01:32:41 -0500

    fix: favorites css polish

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -740,7 +740,7 @@ const Dashboard: React.FC<DashboardProps> = ({
                         <>
                           <button
                             onClick={() => useFavoritedBreakdown(favorite)}
-                            className="px-4 py-2 pr-16 bg-gray-700 text-gray-300 rounded-xl text-sm font-medium transition-all border border-gray-600 active:scale-95 active:bg-gray-500"
+                            className={`px-2 py-1 bg-gray-700 text-gray-300 rounded-xl text-sm font-medium transition-all border border-gray-600 active:scale-95 active:bg-gray-500 ${favorite.userId === user?.id ? 'pr-16' : ''}`}
                           >
                             <span className="text-gray-300">{favorite.name}</span>
                           </button>
