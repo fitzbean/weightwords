5ece59f1616b9f3a4649458310d1e698be42cddf
Brad Lima <blima@grovergaming.com>
2026-02-15 23:53:17 -0500

    fix: camera

--- components/NutritionLabelScanner.tsx
+++ components/NutritionLabelScanner.tsx
@@ -26,6 +26,7 @@ const NutritionLabelScanner: React.FC<NutritionLabelScannerProps> = ({ isOpen, o
     console.log('resetState called');
     setCaptureStep('product');
     setProductImage(null);
+    setIsProcessing(false);
     setError(null);
     setButtonPressed(null);
   }, []);
